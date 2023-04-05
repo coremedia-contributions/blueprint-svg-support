a075b25b52151f9792d0d63c77cae8acc994ffdc
Sebastian <sebastian.buettner@coremedia.com>
2023-04-05 08:42:42 +0200

    Adjusted styling for integration with CK5 rich text fields

--- apps/studio-client/apps/main/svg-studio-plugin/jangaroo.config.js
+++ apps/studio-client/apps/main/svg-studio-plugin/jangaroo.config.js
@@ -8,6 +8,13 @@ module.exports = jangarooConfig({
   sencha: {
     name: "com.coremedia.blueprint__svg-studio-plugin",
     namespace: "com.coremedia.blueprint.svg.studio",
+    css: [
+      {
+        path: "resources/css/svg-studio-plugin.css",
+        bundle: false,
+        includeInBundle: false,
+      },
+    ],
     studioPlugins: [
       {
         mainClass: "com.coremedia.blueprint.svg.studio.SvgStudioPlugin",
