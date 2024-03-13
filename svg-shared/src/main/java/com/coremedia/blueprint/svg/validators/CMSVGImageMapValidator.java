61214a367b718207d2cbb8f752613adc7dfcba76
Sebastian Büttner <sebastian.buettner@coremedia.com>
2024-03-13 14:44:48 +0100

    Validator clean up

--- svg-shared/src/main/java/com/coremedia/blueprint/svg/validators/CMSVGImageMapValidator.java
+++ svg-shared/src/main/java/com/coremedia/blueprint/svg/validators/CMSVGImageMapValidator.java
@@ -1,36 +1,23 @@
 package com.coremedia.blueprint.svg.validators;
 
 import com.coremedia.cap.content.Content;
-import com.coremedia.cap.content.ContentType;
 import com.coremedia.rest.cap.validation.ContentTypeValidatorBase;
 import com.coremedia.rest.validation.Issues;
 import com.coremedia.rest.validation.Severity;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import edu.umd.cs.findbugs.annotations.NonNull;
-import edu.umd.cs.findbugs.annotations.Nullable;
 
 /**
  * Validator form CMImageMaps.
- *
+ * <p>
  * The validator checks if the picture property of the image map is set to an CMSVG since this is not supported for now.
  */
 public class CMSVGImageMapValidator extends ContentTypeValidatorBase {
-
-  @JsonCreator
-  public CMSVGImageMapValidator(@JsonProperty(value = "content-type", required = true) @NonNull ContentType type,
-                                @JsonProperty(value = "subtypes") @Nullable Boolean isValidatingSubtypes) {
-    super();
-    setContentType(type.toString());
-    setValidatingSubtypes(isValidatingSubtypes);
-  }
 
   @Override
   public void validate(Content content, Issues issues) {
     Content linkedPicture = content.getLink("pictures");
 
     if (linkedPicture != null && linkedPicture.getType().isSubtypeOf("CMSVG")) {
-      issues.addIssue(Severity.ERROR, "pictures","cmsvg_not_supported_as_imagemap_picture");
+      issues.addIssue(Severity.ERROR, "pictures", "cmsvg_not_supported_as_imagemap_picture");
     }
 
   }
--- svg-shared/src/main/java/com/coremedia/blueprint/svg/validators/CMSVGValidator.java
+++ svg-shared/src/main/java/com/coremedia/blueprint/svg/validators/CMSVGValidator.java
@@ -2,32 +2,19 @@ package com.coremedia.blueprint.svg.validators;
 
 import com.coremedia.cap.common.Blob;
 import com.coremedia.cap.content.Content;
-import com.coremedia.cap.content.ContentType;
 import com.coremedia.rest.cap.validation.ContentTypeValidatorBase;
 import com.coremedia.rest.validation.Issues;
 import com.coremedia.rest.validation.Severity;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import edu.umd.cs.findbugs.annotations.NonNull;
-import edu.umd.cs.findbugs.annotations.Nullable;
 import org.apache.commons.lang3.StringUtils;
 
 /**
  * Validator for CMSVG content.
- *
+ * <p>
  * Performs the following checks:
  * - Check if either data or dataUrl is set
  * - Check if data is set when the inlineCode equals true
  */
 public class CMSVGValidator extends ContentTypeValidatorBase {
-
-  @JsonCreator
-  public CMSVGValidator(@JsonProperty(value = "content-type", required = true) @NonNull ContentType type,
-                           @JsonProperty(value = "subtypes") @Nullable Boolean isValidatingSubtypes) {
-    super();
-    setContentType(type.toString());
-    setValidatingSubtypes(isValidatingSubtypes);
-  }
 
   @Override
   public void validate(Content content, Issues issues) {
@@ -36,8 +23,8 @@ public class CMSVGValidator extends ContentTypeValidatorBase {
     boolean asInlineCode = content.getBoolean("inlineCode");
 
     if (data == null && StringUtils.isBlank(dataUrl)) {
-      issues.addIssue(Severity.ERROR, "data","data_and_datUrl_empty");
-      issues.addIssue(Severity.ERROR, "dataUrl","data_and_datUrl_empty");
+      issues.addIssue(Severity.ERROR, "data", "data_and_datUrl_empty");
+      issues.addIssue(Severity.ERROR, "dataUrl", "data_and_datUrl_empty");
     }
 
     if (data == null && asInlineCode) {
