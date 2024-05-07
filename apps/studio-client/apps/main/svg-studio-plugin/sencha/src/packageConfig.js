20ca5f55e2d8d72386e29ac93484a61270325f98
Sebastian Büttner <sebastian.buettner@coremedia.com>
2024-05-07 16:21:03 +0200

    Removed obsolete code

--- apps/studio-client/apps/main/svg-studio-plugin/sencha/src/packageConfig.js
+++
@@ -1,3 +0,0 @@
-// START - Adding global resources to ext manifest
-Ext.registerGlobalResources({ "ckeditor.plugin.cmsvgsupport": "<@com.coremedia.blueprint__svg-studio-plugin>ckeditor/plugins/cmsvgsupport/plugin.js" });
-// END - Adding global resources to ext manifest
